ac7bc962d9eb0bc5f67cbd6b02ab22134d56f13f
Lee <leecocky@hotmail.com>
2020-01-10 11:18:18 +0100

    Updates

---
+++ precache-manifest.8dd6bcb8336adc1361e3bbf6e19e45d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be2616125ee44dee2a9000e823059aa3",
+    "url": "/react-countries/index.html"
+  },
+  {
+    "revision": "52cefe7be65ef9db32d8",
+    "url": "/react-countries/static/css/main.99a3e1c4.chunk.css"
+  },
+  {
+    "revision": "ec9618d167c65262d6d9",
+    "url": "/react-countries/static/js/2.f4896203.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-countries/static/js/2.f4896203.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52cefe7be65ef9db32d8",
+    "url": "/react-countries/static/js/main.723ccea6.chunk.js"
+  },
+  {
+    "revision": "88344383bd2fb99a9699",
+    "url": "/react-countries/static/js/runtime-main.d50c9051.js"
+  }
+]);
